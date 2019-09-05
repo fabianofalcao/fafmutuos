29147a0f9369fe8553b894f5f59291325600e705
Fabiano Alves Falcao <fabiano.falcao@IFMG.EDU.BR>
2019-09-05 10:14:44 -0300

    ajuste menu principal - proposta de emprestimos

--- resources/views/admin/master/app.blade.php
+++ resources/views/admin/master/app.blade.php
@@ -133,7 +133,14 @@
                             </p>
                         </a>
                     </li>
-
+                    <li class="nav-item">
+                        <a href="{{route('admin.debtors.index')}}" class="nav-link">
+                            <i class="nav-icon fas fa-dollar-sign"></i>
+                            <p>
+                                Propostas de empréstimos
+                            </p>
+                        </a>
+                    </li>
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
